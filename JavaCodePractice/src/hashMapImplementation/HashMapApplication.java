fb6259a98419db7de4d055751bb2ab8307cdf10b
vivek adarsh <vivek.adarsh@wootz.io>
2022-08-27 16:08:11 +0530

    HashMapImplementation

--- JavaCodePractice/src/hashMapImplementation/HashMapApplication.java
+++ JavaCodePractice/src/hashMapImplementation/HashMapApplication.java
@@ -1,10 +1,22 @@
 package hashMapImplementation;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapApplication {
 
     public static void main(String[] args) {
-        HashMap hm = new HashMap<>();
+        HashMap<Integer,String> hm = new HashMap<Integer,String>();
+
+        hm.put(1, "vivek");
+        hm.put(1, "Abhishek");
+
+        // System.out.println(hm.keySet());
+      //  System.out.println(hm.values());
+
+        for (Map.Entry<Integer, String> entry : hm.entrySet()) {
+            System.out.println(entry);
+        }
+
     }
 }
